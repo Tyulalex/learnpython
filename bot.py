decc4839112efbb6040ce54c5fc9af437e039107
Tatiana Karabut <tyulalex@gmail.com>
2017-05-27 18:16:38 +0300

    Some changes in bot

--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
+import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+                    )
 
 
 def main():
@@ -22,8 +26,9 @@ def show_error(bot, update, error):
     print(error)
 
 def talk_to_me(bot, update):
-    print(update.message.text)
-    bot.sendMessage(update.message.chat_id, update.message.text)
+    user_text = update.message.text 
+    print(user_text)
+    update.message.reply_text(user_text)
 
 
 main()
