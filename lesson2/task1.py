96539d2b01e02ca0412bc940e90daa230831f009
Tatiana Karabut <tyulalex@gmail.com>
2017-06-01 00:35:56 +0300

    lesson2 task1 version 2

--- lesson2/task1.py
+++ lesson2/task1.py
@@ -1,8 +1,14 @@
 import sys
 
+PROF_TO_AGE_RANGE_MAP = {'Детский сад': [0, 6],
+                         'Школа': [7, 16],
+                         'Университет': [17, 22],
+                         'Работа': [23, 69],
+                         'Пенсия': [70, 120]}
 
-def is_raw_age_valid(raw_age):
-    if raw_age.isdigit() and int(raw_age) < 120:
+
+def is_raw_age_valid(raw_age, max_age=120):
+    if raw_age.isdigit() and int(raw_age) < max_age:
         return True
 
 
@@ -12,13 +18,11 @@ if __name__ == '__main__':
         print("Введено неправильно значение, пожалуйста, введите корректный возраст")
         sys.exit(1)
     age = int(raw_age)
-    if age < 6:
-        print("Детский сад")
-    elif age < 16:
-        print("Школа")
-    elif age < 22:
-        print("Университет")
-    elif age < 70:
-        print("Работа")
-    else:
-        print("Пенсия")
+
+    for profession, age_range in PROF_TO_AGE_RANGE_MAP.items():
+        if age_range[0] <= age <= age_range[1]:
+            print(profession)
+            sys.exit(1)
+    print(
+        "Род занятий не определен для вашего возраста: {age}". format(
+            age=age))
