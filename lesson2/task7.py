ca32133e0aaffeca52d6ff443522debe455d6584
Tatiana Karabut <tyulalex@gmail.com>
2017-06-03 11:53:33 +0300

    task7

--- lesson2/task7.py
+++ lesson2/task7.py
@@ -5,6 +5,14 @@ def ask_user():
     actual_answer = ''
     while actual_answer != expected_answer:
         actual_answer = input("Как дела\n")
+        return actual_answer
+
+
+def get_answer():
+    expected_answer = "Пока"
+    actual_answer = ''
+    while actual_answer != expected_answer:
+        actual_answer = ask_user()
 
 if __name__ == '__main__':
-    ask_user()
+    get_answer()
