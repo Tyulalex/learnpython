4b118b75150a118aac9ad3b1af292f8fd9d31cea
Tatiana Karabut <tyulalex@gmail.com>
2017-06-13 00:15:52 +0300

    some new tasks

--- lesson4/bus_stops_task.py
+++ lesson4/bus_stops_task.py
@@ -1,7 +1,7 @@
 import csv
 
 
-def get_most_common_value_per_column(file, column_name):
+def max_station_group_by_column(file, column_name):
     with open(file, 'r', encoding='cp1251') as f:
         reader = csv.DictReader(f, delimiter=';')
         street_station_count_map = {}
@@ -17,5 +17,5 @@ def get_most_common_value_per_column(file, column_name):
 
 if __name__ == '__main__':
     csv_file = 'data-398-2017-06-05.csv'
-    most_common_street = get_most_common_value_per_column(csv_file, "Street")
+    most_common_street = max_station_group_by_column(csv_file, "Street")
     print("Улица, на которой больше всего остановок: {street}".format(street=most_common_street))
